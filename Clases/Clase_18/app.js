7d2e81992d846a4752ad8f9a368c0904f534379e
Mauro Reimunto <mauroagustinreimunto@gmail.com>
2021-12-08 21:34:55 -0300

    subo clase de js

---
+++ Clases/Clase_18/app.js
@@ -0,0 +1,119 @@
+//Ejercicio 0:
+    // let nombre = prompt("¿Cuál es tu nombre?");
+    // alert("Hola " + nombre);
+    // let apellido = prompt("¿Cual es tu apellido?");
+    // alert("Hola "+nombre + ' ' + apellido);
+
+//Ejercicio 1:
+    // let firstNumber = prompt("Elije tu primer número");
+    // let secondNumber = prompt("Elije tu segundo número");
+    // let resultado = parseFloat(firstNumber) + parseFloat(secondNumber);
+    // alert("La suma de ambos números es: " + resultado);
+
+//Ejercicio 2:
+    //let edad = prompt("¿Cuantos años cumplís o cumpliste este año?");
+    //let actual = "2021";
+    //let resta = parseFloat(actual) - parseFloat(ed""ad);
+    //alert ("Naciste en el año " + resta);
+
+//Ejercicio 3:
+    //let cuenta = prompt("Ingrese su número de cuenta:");
+    //let dineroCuenta = "1000";
+    //alert("Su saldo es de: " + parseFloat(dineroCuenta));
+    //let egreso = prompt("¿Cuánto desea retirar?");
+    //let saldoFinal = parseFloat(dineroCuenta) - parseFloat(egreso);
+    //alert("Su saldo restante es: $" + saldoFinal);
+
+//Ejercicio 4:
+    //let notaUno = prompt("Ingrese su primer nota: ");
+    //let notaDos = prompt("Ingrese su segunda nota:");
+    //let notaTres = prompt("Ingrese su tercer nota:");
+    //let promedio = (parseFloat(notaUno) + parseFloat(notaDos) + parseFloat(notaTres))/3;
+    //alert("Su promedio final es: "+ promedio) ;
+
+//Ejercicio 5:
+//metros a pies= 3.28084 -- metro a pulgada= 39.3701
+    // let metros = prompt("Convertir medida");
+    // let pulgadas = parseFloat(metros) * 39.3701;
+    // let pies = parseFloat(metros) * 3.28084;
+    // let cm = parseFloat(metros) * 100;
+    // alert( parseFloat(metros) + " metros es igual a: " + parseFloat(pulgadas) +" pulgadas, " + parseFloat(pies) +" pies y " + parseFloat(cm) + " cm.");
+
+
+//Ejercicio 6:
+    //let firstNumber = prompt("Elije tu primer número");
+    //let secondNumber = prompt("Elije tu segundo número");
+    //let suma = parseFloat(firstNumber) + parseFloat(secondNumber);
+    //alert("El resultado de la suma es: " + suma);
+    //let multiplicador = prompt("Elije un número multiplicador");
+    //alert("El numero final es: " + suma * multiplicador);
+
+//Ejercicio 7:
+    //let minutos = prompt("Ingrese minuto(s) a convertir:");
+    //let convertir = parseFloat(minutos)*  60;
+    //alert(minutos+" minutos equivale a: " + convertir + " segundos.") ;
+
+//Ejercicio 8:
+// let base = prompt("Ingrese la base del rectángulo:");
+// let altura = prompt("Ingrese la altura del rectángulo:");
+// alert("Su área es de: " + parseFloat(base) * parseFloat(altura));
+
+//Ejercicio 9:
+// let base = prompt("Ingrese la base del triángulo:");
+// let altura = prompt("Ingrese la altura del triángulo:");
+// alert("Su área es de: " + (parseFloat(base) * parseFloat(altura)) / 2);
+
+// Ejercicio 10
+// let monto = prompt('Ingrese el monto: ');
+// let descuento = prompt('Ingrese el descuento: ');
+// let precioFinal = parseFloat(monto) - parseFloat(descuento);
+// alert('El precio final con el descuento aplicado es de: ' + precioFinal);
+
+
+//Ejercicio 11:
+// let nombre = prompt("Ingrese su nombre:");
+// let edad = prompt("Ingrese la edad que cumplió o cumplirá en 2021:");
+// alert("Hola " + nombre + " usted nació en el año: " + (2021 - (parseFloat(edad))));
+
+// Ejercicio 12
+// let nombreUsuario = prompt("¿Cual es tu nombre?");
+// let temperaturaCelsius = prompt("¿Que valor quiere convertir? (Cº)");
+// alert(nombreUsuario + " el valor es de: " + ((parseFloat(temperaturaCelsius) * 1.8) +32 )+ "ºF");
+
+
+//Ejercicio 13:
+// let nombre1 = prompt("Ingrese su nombre:");
+// let tempF = prompt("Ingrese la temperatura en Farenheit:");
+// let tempC = (parseFloat(tempF)-32) * 1.8;
+// alert("Hola "+ nombre1 +"! la temperatura equivale a "+ tempC+ " Celsius.");
+
+
+
+
+
+// Ejercicio 14:
+    //let numero = prompt(" Ingrese un numero");
+    //let suma = parseFloat(numero)+5;
+    //let multiplicacion = parseFloat(suma)*10;
+    //let resto = parseFloat(multiplicacion)/3;
+    //alert("El resultado es : " + resto);
+
+
+
+// BONUS
+// Ejercicio 0
+let num1 = prompt('Ingrese un numero');
+let num2 = prompt('Ingrese otro numero');
+let suma = num1 + num2;
+alert('La suma entre ' + num1 + ' y ' + num2 + 'es ' + suma);
+
+
+// Ejercicio 1
+
+vamoo el teeeaam ;)
+
+// Ejercicio 2
+
+
+
+// Ejercicio 3
